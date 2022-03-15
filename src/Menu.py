54d9505a3c0961c3f02cd89832229d85679ba426
Jaffar3984 <sjraza2019@gmail.com>
2022-03-15 19:01:18 -0400

    com

---
+++ src/Menu.py
@@ -0,0 +1,56 @@
+import pygame, sys
+
+from pygame.locals import *
+
+
+
+class Menu:
+
+    def __init__(self, surface, background_img, buttonInitImg, buttonFinalImg):
+        self.surface = surface
+        self.background_img = background_img
+        self.buttonInitImg = buttonInitImg
+        self.buttonFinalImg = buttonFinalImg
+
+    def main_menu(self,clock, location_b1, location_b2):
+
+        index = 0
+
+        click = False
+        running = True
+        while running:
+            self.surface.fill((0,0,0))
+
+            mx,my = pygame.mouse.get_pos()
+
+            button_1 = pygame.Rect(location_b1[0], location_b1[1], self.buttonInitImg.get_width(), self.buttonInitImg.get_height())
+            self.surface.blit(self.buttonInitImg,location_b1)
+
+            if button_1.collidepoint(mx,my):
+                self.surface.blit(self.buttonFinalImg, location_b1)
+                if click:
+                    running = False
+                    index = 1
+                    return index
+
+            button_2 = pygame.Rect(location_b2[0], location_b2[0], self.buttonInitImg.get_width(), self.buttonInitImg.get_height())
+            self.surface.blit(self.buttonInitImg, location_b2)
+
+            if button_2.collidepoint(mx,my):
+                self.surface.blit(self.buttonFinalImg, location_b2)
+                if click:
+                    running = False
+                    index = 2
+                    return index
+
+            click = False
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    sys.exit()
+                if event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        click = True
+
+            pygame.display.update()
+            clock.tick(60)
--- src/app.py
+++ src/app.py
@@ -1,8 +1,6 @@
 import sys
 
-import os
-
-import playsound
+from src.Menu import Menu
 
 import pygame
 
@@ -11,8 +9,6 @@ clock = pygame.time.Clock()
 from pygame.locals import *
 
 pygame.init()
-
-pygame.display.set_caption("Le Pygame Window!!")
 
 #HERE YOU CAN CHANGE THE IMAGE UR SOURCING
 backgrd_img = pygame.image.load("images/Rengoku.jpg")
@@ -26,41 +22,60 @@ WINDOW_SIZE = (backgrd_img.get_width(), backgrd_img.get_height())
 
 screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
 
-pygame.mixer.music.load("sounds/music/SYKO_music.wav")
-pygame.mixer.music.play(1)
-
-
 image_index = 0
+
+game_index = 0
+
+menu = Menu(screen,backgrd_img,img_2,img_3)
+
+
+def game(image_index,screen):
+    pygame.display.set_caption("Le Pygame Window!!")
+    pygame.mixer.music.load("sounds/music/SYKO_music.wav")
+    pygame.mixer.music.play(-1)
+    running = True
+    while running:
+
+        if image_index == 6:
+            image_index = 0
+        if image_index == 0:
+            screen.fill((0,0,0))
+            screen.blit(backgrd_img, (0, 0))
+            image_index += 1
+        if image_index == 2:
+            screen.fill((0,0,0))
+            screen.blit(img_2, (0, 0))
+            image_index += 1
+        if image_index == 4:
+            screen.fill((0,0,0))
+            screen.blit(img_3, (0, 0))
+            image_index += 1
+
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    image_index += 1
+                if event.key == K_ESCAPE:
+                    running = False
+                    return 0
+
+
+        pygame.display.update()
+        clock.tick(60)
+
 
 while True:
 
-    print(image_index)
-
-    if image_index == 6:
-        image_index = 0
-    if image_index == 0:
-        screen.fill((0,0,0))
-        screen.blit(backgrd_img, (0, 0))
-        image_index += 1
-    if image_index == 2:
-        screen.fill((0,0,0))
-        screen.blit(img_2, (0, 0))
-        image_index += 1
-    if image_index == 4:
-        screen.fill((0,0,0))
-        screen.blit(img_3, (0, 0))
-        image_index += 1
+    if game_index == 0:
+        pygame.mixer.quit()
+        game_index = menu.main_menu(clock,(50,50), (100,100))
+    if game_index == 1:
+        pygame.mixer.init()
+        game_index = game(image_index,screen)
 
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            sys.exit()
-        if event.type == KEYDOWN:
-            if event.key == K_UP:
-                image_index += 1
-
-
-    pygame.display.update()
-    clock.tick(60)
 
 
