733bb56548f0019d91238fb5283ad655951a0027
Ember Hunter <Peterfam53310@yahoo.com>
2022-03-23 19:07:12 -0400

    jm

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ clock = pygame.time.Clock()
 
 pygame.init()
 
-screen = pygame.display.set_mode(constants.WINDOWSIZE,0,32)
+screen = pygame.display.set_mode(constants.WINDOWSIZE, 0, 32)
 
-level3 = level.Level3(clock,screen,"map/level_3/map_0.txt",constants.level3_tile_image_list)
+level3 = level.Level3(clock, screen, "map/level_3/map_0.txt", constants.level3_tile_image_list)
 
 
 while True:
