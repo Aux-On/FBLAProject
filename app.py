3da5380b983f3d343aca2fa7e127524ed6f38c4c
Jaffar3984 <sjraza2019@gmail.com>
2022-03-28 12:53:21 -0400

    com

--- app.py
+++ app.py
@@ -17,16 +17,8 @@ level3 = level.Level3(clock, screen, "map/level_3/map_0.txt", constants.level3_t
 
 ##### Main Menu
 
-menu_image_raw = pygame.image.load("images/Template.jpg")
-yfactor = display.get_height()/menu_image_raw.get_height()
-menu_image = pygame.transform.scale(menu_image_raw,[menu_image_raw.get_width()*yfactor,menu_image_raw.get_width()*yfactor])
+menu_image = pygame.image.load("images/Menuu.png")
 
-button1_image = pygame.transform.scale(pygame.image.load("images/button1.jpg"),[pygame.image.load("images/button1.jpg").get_width()*yfactor,pygame.image.load("images/button1.jpg").get_height()*yfactor])
-button2_image = pygame.transform.scale(pygame.image.load("images/button2.jpg"),[pygame.image.load("images/button2.jpg").get_width()*yfactor,pygame.image.load("images/button2.jpg").get_height()*yfactor])
-
-
-base_button_location = [13.2,18]
-adjusted_button_location = [base_button_location[0]*yfactor,base_button_location[1]*yfactor]
 
 click = False
 
@@ -35,17 +27,20 @@ surface_scale_factor = [constants.WINDOWSIZE[0]/constants.surface_size[0],consta
 
 pygame.mixer.music.load("sounds/music/WS-Calm-flat-_1.wav")
 pygame.mixer.music.play(-1)
+
+
 while True:
     display.fill((menu_image.get_at((1,1))[0],menu_image.get_at((1,1))[1],menu_image.get_at((1,1))[2]))
-    display.blit(menu_image,[(display.get_width()/2)-(menu_image.get_width()/2),0])
+    display.blit(menu_image,[0,0])
 
     mx,my = pygame.mouse.get_pos()
+    mx = mx/5
+    my = my/5
 
-    print(surface_scale_factor)
+    print((mx,my))
 
-    button_rect = pygame.rect.Rect(adjusted_button_location[0]*surface_scale_factor[0],adjusted_button_location[1]*surface_scale_factor[1],button1_image.get_width()*yfactor*surface_scale_factor[0],button1_image.get_height()*surface_scale_factor[1])
+    button_rect = pygame.rect.Rect(0,0,16,16)
     if button_rect.collidepoint(mx,my):
-        menu_image.blit(button2_image,adjusted_button_location)
         if click:
             pygame.mixer.music.stop()
             pygame.mixer.music.unload()
@@ -55,8 +50,6 @@ while True:
             level1.game()
             pygame.mixer.music.load("sounds/music/WS-Calm-flat-_1.wav")
             pygame.mixer.music.play(-1)
-    else:
-        menu_image.blit(button1_image,adjusted_button_location)
 
 
 
